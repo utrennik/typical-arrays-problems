9b55c19a403a56ddf4bda53d1630568c8acd59dd
Utrennik <utrennik@list.ru>
2021-01-27 20:52:24 +0300

    feat: create functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+exports.min = function min(array) {
+    if (!array || !array.length) return 0;
+    return Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || !array.length) return 0;
+    return Math.max(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || !array.length) return 0;
+    let sum = array.reduce((sum, item) => {
+        return (sum += item);
+    }, 0);
+    return sum / array.length;
+};
